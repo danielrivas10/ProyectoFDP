e0a5d687e804b2a90be313d7753b55095922ec43
danielrivas10 <00071126@uca.edu.sv>
2026-06-13 16:45:21 -0600

    git  commit -m ":memo: agregando codigo de flujograma "

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,167 +1,295 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-string name, bo, Name_pokemon;
-string x, op;
-string Moves_s[4] = {"1. Pistola Agua: Ataque a distancia. Resta 15 HP al enemigo",
-                     "2. Burbuja: Reduce la velocidad y resta 10 HP al enemigo",
-                     "3. Placaje: Ataque fisico. Resta 12 HP al enemigo",
-                     "4. Hidrobomba: Ataque especial. Resta 30 HP al enemigo"};
-string Moves_c[4] = {"1. Ascuas: Ataque de fuego. Resta 15 HP al enemigo",
-                     "2. Lanzallamas: Ataque intenso. Resta 30 HP al enemigo ",
-                     "3. Arañazo: Ataque rapido. Resta 10 HP al enemigo",
-                     "4. Giro Fuego: Atrapa al enemigo y resta 20 HP"};
-string Moves_b[4] = {"1. Latigo Cepa: Ataque con lianas. Resta 15 HP al enemigo",
-                     "2. Hoja Afilada: Ataque rapido. Resta 20 HP al enemigo",
-                     "3. Drenadoras: Absorbe energia y resta 10 HP al enemigo",
-                     "4. Placaje: Ataque fisico. Resta 12 HP al enemigo"};
+struct Pokemon {
 
-void menu();
-void show_moves();
-void show_description();
-void bienvenida();
-int main()
-{
-    cout << "Bienvenido a pokemon xyz " << endl;
-    cout << "Presione cualquier boton para continuar" << endl;
+    string nombre, movimientos;
+    int vida, experiencia, nivel, fuerza;
+};
 
-    cin >> bo;
-    cout << "En la tierra perdida de Kanto, habia un joven que buscaba ser el mejor de todos. " << endl
-         << "Y su nombre era: ";
-    cin >> name;
-    cout << endl;
-
-    cout << "El doctor Osaka le dijo: Hola joven " << name << ". He oido que quieres comenzar con tu aventura pokemon. Pero antes de necesitas tu primer pokemon." << endl
-         << "Presione: " << endl
-         << "1 para seleccionar a Squirtle " << endl
-         << "2 para seleccionar a Charmander " << endl
-         << "3 para seleccionar a Bulbasaur" << endl;
-    cin >> x;
-
-    while (x != "1" && x != "2" && x != "3")
-    {
-        cout << "Selecciona una opcion posible" << endl;
-        cout << "Presione: " << endl
-             << "1 para seleccionar a Squirtle " << endl
-             << "2 para seleccionar a Charmander " << endl
-             << "3 para seleccionar a Bulbasaur " << endl;
-        cin >> x;
+void GuardarPartida(const Pokemon& pokemon){
+    ofstream archivo("datos_partida.txt");
+    if (archivo.is_open()) {
+        archivo << pokemon.nombre << endl;
+        archivo << pokemon.vida << endl;
+        archivo << pokemon.experiencia << endl;
+        archivo << pokemon.nivel << endl;
+        archivo << pokemon.fuerza << endl;
+        archivo.close();
+        cout << "Partida guardada." << endl;
     }
-
-    if (x == "1")
-    {
-        Name_pokemon = "Squirtle";
-        bienvenida();
-        do
-        {
-            menu();
-        } while (op != "x");
-    }
-
-    else if (x == "2")
-    {
-        Name_pokemon = "Charmander";
-        bienvenida();
-        do
-        {
-            menu();
-        } while (op != "x");
-    }
-
-    else if (x == "3")
-    {
-        Name_pokemon = "Bulbasaur";
-        bienvenida();
-        do
-        {
-            menu();
-        } while (op != "x");
-    }
-    cout << "Dr Osaka: Muy bien joven " << name << ". Ahora que ya conoces a tu pokemon y sus movimientos, es hora de comenzar tu aventura. ¡Buena suerte!" << endl;
-    return 0;
-}
-
-void menu()
-{
-    cout << "\n"
-         << endl;
-    cout << "=================================================" << endl;
-    cout << Name_pokemon << " seleccionado." << endl;
-    cout << "Presiona A para ver tus movimientos." << endl;
-    cout << "Presiona B para ver la descripcion de tu pokemon" << endl;
-    cout << "Si estás listo para luchar, presiona x" << endl;
-    cout << "=================================================" << endl;
-    cin >> op;
-
-    while (op != "A" && op != "B" && op != "x")
-    {
-        cout << "Ingrese una accion valida" << endl;
-        cin >> op;
-    }
-
-    if (op == "A")
-    {
-        show_moves();
-    }
-    else if (op == "B")
-    {
-        show_description();
+    else {
+        cout << "No se pudo guardar la partida." << endl;
     }
 }
 
-void show_moves()
+bool CargarPartida(Pokemon& pokemon) {
+    ifstream archivo("datos_partida.txt");
+    if (archivo.is_open()) {
+        getline(archivo, pokemon.nombre);
+        archivo >> pokemon.vida;
+        archivo >> pokemon.experiencia;
+        archivo >> pokemon.nivel;
+        archivo >> pokemon.movimientos;
+        archivo.close();
+        return true;
+
+    }
+    return false;
+}
+void MostrarPokemon(Pokemon& pokemon){
+    cout << endl << "==== Estadisticas del pokemon seleccionado ====" << endl;
+    cout << "Pokemon elegido: " << pokemon.nombre << endl;
+    cout << "Vida: " << pokemon.vida << endl;
+    cout << "Fuerza: " << pokemon.fuerza << endl;
+    cout << "Experiencia: " << pokemon.experiencia << endl;
+    cout << "Nivel: " << pokemon.nivel << endl;
+    cout << "=====================" << endl; 
+}
+
+void MovimientosPokemon(const Pokemon& pokemon)
 {
+    string ataque;
+    cout << "Ataques de: " << pokemon.nombre << endl;
+    string Moves_s[4] = {"A. Pistola Agua: Ataque a distancia. 15 puntos de ataque base",
+                        "B. Burbuja: 10 puntos de ataque base",
+                        "C. Placaje: Ataque fisico. 12 puntos de ataque base",
+                        "D. Hidrobomba: Ataque especial. 30 puntos de ataque base"};
+    string Moves_c[4] = {"A. Ascuas: Ataque de fuego. 15 puntos de ataque base",
+                        "B. Lanzallamas: Ataque intenso. 30 puntos de ataque base",
+                        "C. Araniazo: Ataque rapido. 10 puntos de ataque base",
+                        "D. Giro Fuego: 20 puntos de ataque base"};
+    string Moves_b[4] = {"A. Latigo Cepa: Ataque con lianas. 15 puntos de ataque base",
+                        "B. Hoja Afilada: Ataque rapido. 20 puntos de ataque base",
+                        "C. Drenadoras: Roba un poco de vida y hace 30 puntos de ataque base",
+                        "D. Placaje: Ataque fisico. 12 puntos de ataque base"};
     for (int i = 0; i <= 3; i++)
     {
-        if (Name_pokemon == "Squirtle")
+        if (pokemon.nombre == "Squirtle"){
             cout << Moves_s[i] << endl;
-        else if (Name_pokemon == "Charmander")
+        }
+        else if (pokemon.nombre == "Charmander")
         {
             cout << Moves_c[i] << endl;
         }
-        else if (Name_pokemon == "Bulbasaur")
+        else if (pokemon.nombre == "Bulbasaur")
         {
             cout << Moves_b[i] << endl;
         }
     }
-    cout << "Si deseas regresar al menu, presiona z" << endl;
-    cin >> op;
-    while (op != "z")
-    {
-        cout << "Ingrese una accion valida" << endl;
-        cin >> op;
+}
+
+void Combate(Pokemon& pokemon){
+srand(time(0));
+    int vidaPokemonEnemigo = 30 + rand() % 10;
+    int atkPokemonEnemigo = 10 + rand () % 5;
+    char opcion;
+    string ataque;
+
+    cout << endl << "Un Pokemon salvaje con: " << vidaPokemonEnemigo << " puntos de vida y " << atkPokemonEnemigo << " puntos de ataque " << "se acerca!" << endl;
+    while (pokemon.vida > 0 && vidaPokemonEnemigo > 0) {
+
+        // El prota ataca primero
+        cout << "Selecciona un ataque: " << endl << endl;
+        MovimientosPokemon (pokemon);
+        cin >> opcion;
+
+        if (opcion == 'A' || opcion == 'a'){
+            if (pokemon.nombre == "Squirtle") {
+                ataque = "Pistola agua";
+                vidaPokemonEnemigo -= 15;
+            }
+                else if (pokemon.nombre == "Charmander") {
+                    ataque = "Ascuas";
+                    vidaPokemonEnemigo -= 15;
+                }
+                    else if (pokemon.nombre == "Bulbasaur") {
+                        ataque = "Latigo Cepa";
+                        vidaPokemonEnemigo -= 15;
+                    }
+        } 
+        else if (opcion == 'B' || opcion == 'b'){
+            if (pokemon.nombre == "Squirtle") {
+                ataque = "Burbuja";
+                vidaPokemonEnemigo -= 10;
+            }
+                else if (pokemon.nombre == "Charmander") {
+                    ataque = "Lanzallamas";
+                    vidaPokemonEnemigo -= 30;
+                }
+                    else if (pokemon.nombre == "Bulbasaur") {
+                        ataque = "Hoja Afilada";
+                        vidaPokemonEnemigo -= 20;
+                    }
+        } 
+        else if (opcion == 'C' || opcion == 'c') {
+            if (pokemon.nombre == "Squirtle") {
+                ataque = "Placaje";
+                vidaPokemonEnemigo -= 12;
+            }
+                else if (pokemon.nombre == "Charmander") {
+                    ataque = "Araniazo";
+                    vidaPokemonEnemigo -= 10;
+                }
+                    else if (pokemon.nombre == "Bulbasaur") {
+                        ataque = "Drenadoras";
+                        vidaPokemonEnemigo -= 30;
+                        pokemon.vida += pokemon.fuerza % 10;
+                    }
+        } 
+        else if (opcion == 'D' || opcion == 'd') {
+            if (pokemon.nombre == "Squirtle") {
+                ataque = "Hidrobomba";
+                vidaPokemonEnemigo -= 30;
+            }
+                else if (pokemon.nombre == "Charmander") {
+                    ataque = "Giro Fuego";
+                    vidaPokemonEnemigo -= 20;
+                }
+                    else if (pokemon.nombre == "Bulbasaur") {
+                        ataque = "Placaje";
+                        vidaPokemonEnemigo -= 12;
+                    }
+        }
+        cout << pokemon.nombre << " utiliza " << ataque << endl << endl;
+        cout << "La vida del enemigo ha bajado a " << vidaPokemonEnemigo << "!" << endl << endl;
+        if (vidaPokemonEnemigo <=0){
+
+            cout << "Haz vencido al enemigo!" << endl << endl;
+            pokemon.experiencia += 10;
+            if (pokemon.experiencia >= 30){
+                pokemon.nivel++;
+                pokemon.vida += 10;
+                pokemon.fuerza += 5;
+            cout << "Haz subido de nivel! " << "Vida y fuerza aumentados!" << endl;
+            }
+            MostrarPokemon(pokemon);
+            return;
+        }
+
+        // El enemigo ataca de vuelta (pendiente)
+        pokemon.vida -= atkPokemonEnemigo;
+        cout << "El enemigo ha contraatacado! " << "La vida de " << pokemon.nombre << " se reduce a " << pokemon.vida << "!" << endl << endl;
+
+        if (pokemon.vida <=0) {
+
+            cout << "Has sido derrotado. Fin del juego" << endl;
+        return;       
+        }
+
     }
 }
 
-void show_description()
-{
-    if (Name_pokemon == "Squirtle")
-    {
-        cout << Name_pokemon << " es un pokemon tipo agua muy alegre, sin Sin embargo no dejes que su encanto te engane, ya que es capaz de utilizar multiples ataques tipo agua." << endl;
+void ElegirPokemon(Pokemon& pokemon){
+    char opcion;
+    while (opcion != 'A' && opcion != 'a' && opcion != 'B' && opcion != 'b' && opcion != 'C' && opcion != 'c'){
+        cout << "Elige un Pokemon" << endl
+        << "A para seleccionar a Squirtle " << endl
+        << "B para seleccionar a Charmander " << endl
+        << "C para seleccionar a Bulbasaur" << endl;
+    cin >> opcion;
     }
-    else if (Name_pokemon == "Charmander")
+if (opcion == 'A' || opcion == 'a')
     {
-        cout << Name_pokemon << " es un pokemon tipo fuego, es un pokemon muy valiente y leal a su entrenador. Es capaz de utilizar multiples ataques tipo fuego." << endl;
+        pokemon.nombre = "Squirtle";
+        pokemon.vida = 30;
+        pokemon.fuerza = 18;
+        pokemon.nivel = 4;
+        pokemon.experiencia = 0;
+            MostrarPokemon(pokemon);
     }
-    else if (Name_pokemon == "Bulbasaur")
+
+    else if (opcion == 'B' || opcion == 'b')
     {
-        cout << Name_pokemon << " es un pokemon tipo planta, es un pokemon muy tranquilo y pacifico. Es capaz de utilizar multiples ataques tipo planta." << endl;
+        pokemon.nombre = "Charmander";
+        pokemon.vida = 35;
+        pokemon.fuerza = 22;
+        pokemon.nivel = 5;
+        pokemon.experiencia = 0;
+        MostrarPokemon(pokemon);
     }
-    cout << "Si deseas regresar al menu, presiona z" << endl;
-    cin >> op;
-    while (op != "z")
+
+    else if (opcion == 'C' || opcion == 'c')
     {
-        cout << "Ingrese una accion valida" << endl;
-        cin >> op;
+        pokemon.nombre = "Bulbasaur";
+        pokemon.vida = 25;
+        pokemon.fuerza = 16;
+        pokemon.nivel = 3;
+        pokemon.experiencia = 0;
+        MostrarPokemon(pokemon);
     }
-    if (op == "z")
-    {
-        menu();
+
+}
+
+int main (){
+
+    Pokemon pokemon;
+    char opcion;
+    char opcionmenu;
+
+    cout << "Bienvenido al simulador de batalla pokemon" << endl;
+    cout << "Deseas cargar tu partida anterior? (s/n): ";
+    cin >> opcion;
+
+    if (opcion == 's' || opcion == 'S') {
+        if (CargarPartida(pokemon)){
+        cout << "Partida cargada exitosamente." << endl;
+        MostrarPokemon(pokemon);
+    }
+    else {
+        cout << "No se encontro ningun archivo de guardado. Creando nueva partida..." << endl;
+        ElegirPokemon(pokemon);
     }
 }
 
-void bienvenida()
-{
-    cout << "Usted ha elegido a " << Name_pokemon << endl;
-    cout << "Dr Osaka: Haz elegido muy bien joven " << name << ". Ahora comencemos con lo basico de un combate." << endl;
+    else {
+        ElegirPokemon(pokemon);
+    }
+
+    //Menu Bucle
+    while (pokemon.vida > 0 || opcionmenu != 'A' || opcionmenu != 'a' || opcionmenu != 'B' || opcionmenu != 'b' || opcionmenu != 'C' || opcionmenu != 'c' || opcionmenu != 'D' || opcionmenu != 'd' || opcionmenu != 'E' || opcionmenu != 'e') {
+        cout << endl << "Que deseas hacer?" << endl;
+        cout << "A. Luchar\n";
+        cout << "B. Guardar partida\n";
+        cout << "C. Ver estadisticas de tu pokemon\n";
+        cout << "D. Ver movimientos de tu pokemon\n";   
+        cout << "E. Salir\n";
+        cout << "Opcion: ";
+        cin >> opcionmenu;
+        
+        if (opcionmenu == 'A' || opcionmenu == 'a'){
+
+            Combate(pokemon);
+        }
+
+        else if (opcionmenu == 'B' || opcionmenu == 'b'){
+
+            GuardarPartida(pokemon);
+        }
+
+        else if (opcionmenu == 'C' || opcionmenu == 'c'){
+
+            MostrarPokemon(pokemon);
+        }
+
+        else if (opcionmenu == 'D' || opcionmenu == 'd'){
+
+            MovimientosPokemon(pokemon);
+        }
+        
+        else if (opcionmenu == 'E' || opcionmenu == 'e'){
+
+            cout << "Saliendo del programa. Buen dia." << endl;
+            break;
+        }
+
+        else 
+        cout << "Opcion no valida. Intente de nuevo:" << endl;
+}
+
+return 0;
 }
